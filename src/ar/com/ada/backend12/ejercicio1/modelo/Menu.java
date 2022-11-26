e582d56038247eee0e78526a902aa8b68d9c08a3
franc <francisca.-26@hotmail.com>
2022-11-26 13:01:01 -0300

    Creación de la estructura del main

---
+++ src/ar/com/ada/backend12/ejercicio1/modelo/Menu.java
@@ -0,0 +1,26 @@
+package ar.com.ada.backend12.ejercicio1.modelo;
+
+public class Menu {
+	
+	
+    public Menu() {
+    }
+   
+
+    public void imprimirMenu(){
+           System.out.println("\n" +
+                   "        1.  Listar libros.\n" +
+                   "        2.  Listar revistas.\n" +
+                   "        3.  Listar cubiculos.\n" +
+                   "        4.  Prestar libro. K\n" +
+                   "        5.  Prestar revista.\n" +
+                   "        6.  Prestar cubiculo.\n" +
+                   "        7.  Devolver libro.\n" +
+                   "        8.  Devolver revista.\n" +
+                   "        9.  Devolver cubiculo.\n" +
+                   "        10. Salir." +
+                   "\n");
+       }
+    
+  
+}
